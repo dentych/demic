ae6b77d5906fc3e7c67615dca93552a03fa2c9c0
dentych <dennis@tychsen.me>
2020-12-22 12:36:18 +0100

    Add web

---
+++ web/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>WebSocket example</h1>
+  <p>{{ greeting }}</p>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      greeting: "Hello, world!"
+    }
+  },
+  mounted() {
+    this.socket = new WebSocket("ws://localhost:8081")
+
+    this.socket.addEventListener("open", function (event) {
+      this.socket.send("hello backend")
+    })
+
+    this.socket.addEventListener("message", function (event) {
+      console.log("Received message: ", event.data)
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
